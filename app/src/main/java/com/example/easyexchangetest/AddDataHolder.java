93297030d8dd70c2fee45bca413e5959a338fd2a
Elvis Rahman <elvisrahman4@gmail.com>
2021-01-15 21:14:58 +0530

    Third Commit(Data and image link stored in database)

---
+++ app/src/main/java/com/example/easyexchangetest/AddDataHolder.java
@@ -0,0 +1,45 @@
+package com.example.easyexchangetest;
+
+public class AddDataHolder {
+    String pName, pDescription, email, pUrl;
+
+    public AddDataHolder(String pName, String pDescription, String email, String pUrl) {
+        this.pName = pName;
+        this.pDescription = pDescription;
+        this.email = email;
+        this.pUrl = pUrl;
+    }
+
+    public String getpName() {
+        return pName;
+    }
+
+    public void setpName(String pName) {
+        this.pName = pName;
+    }
+
+    public String getpDescription() {
+        return pDescription;
+    }
+
+    public void setpDescription(String pDescription) {
+        this.pDescription = pDescription;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getpUrl() {
+        return pUrl;
+    }
+
+    public void setpUrl(String pUrl) {
+        this.pUrl = pUrl;
+    }
+}
+
